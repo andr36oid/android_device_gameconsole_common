1db2e8d391edd235f43305881fd900ab1795a34b
sonic011gamer <efimgirjeu@gmail.com>
2025-07-12 11:04:16 +0000

    make this the common repo since we don't do rg351 here

--- rg351mp/BoardConfig.mk
+++
@@ -1,6 +0,0 @@
-PLATFORM_PATH := device/anbernic/rg351mp
-TARGET_SCREEN_DENSITY 		:= 140
-# Camera
-USE_CAMERA_STUB := false
-
-TARGET_KERNEL_CONFIG := lineageos_rg351mp_defconfig
--- rg351p/BoardConfig.mk
+++
@@ -1,6 +0,0 @@
-PLATFORM_PATH := device/anbernic/rg351p
-TARGET_SCREEN_DENSITY 		:= 140
-# Camera
-USE_CAMERA_STUB := false
-
-TARGET_KERNEL_CONFIG := lineageos_rg351p_defconfig
